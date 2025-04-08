277fbf2553825a6dc9facdff17547af26698e6e2
Yousef Kamal <youssefsayed2000@gmail.com>
2025-04-08 06:59:13 +0200

    test and docker

--- backend/tests/account.test.js
+++ backend/tests/account.test.js
@@ -1,29 +1,37 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
 
-describe('Fintech API', () => {
-  let accountId = '';
-  it('should open a new account', async () => {
-    const res = await request(app).post('/api/accounts/open');
-    expect(res.statusCode).toEqual(200);
+describe("Fintech API", () => {
+  let accountId = "";
+  it("should open a new account", async () => {
+    const res = await request(app).post("/api/accounts/open");
+    console.log(res);
+    expect(res.statusCode).toEqual(201);
     expect(res.body.accountId).toBeDefined();
     accountId = res.body.accountId;
   });
 
-  it('should deposit funds', async () => {
-    const res = await request(app).post(`/api/accounts/${accountId}/deposit`).send({ amount: 100 });
+  it("should deposit funds", async () => {
+    const res = await request(app)
+      .post(`/api/accounts/${accountId}/deposit`)
+      .send({ amount: 100 });
+    // console.log(res);
     expect(res.statusCode).toEqual(200);
-    expect(res.body.transactionId).toBeDefined();
+    expect(res.body.txId).toBeDefined();
   });
 
-  it('should withdraw funds', async () => {
-    const res = await request(app).post(`/api/accounts/${accountId}/withdraw`).send({ amount: 50 });
+  it("should withdraw funds", async () => {
+    const res = await request(app)
+      .post(`/api/accounts/${accountId}/withdraw`)
+      .send({ amount: 50 });
+    // console.log(res);
     expect(res.statusCode).toEqual(200);
-    expect(res.body.transactionId).toBeDefined();
+    expect(res.body.txId).toBeDefined();
   });
 
-  it('should get the correct balance', async () => {
+  it("should get the correct balance", async () => {
     const res = await request(app).get(`/api/accounts/${accountId}/balance`);
+    // console.log(res);
     expect(res.statusCode).toEqual(200);
     expect(res.body.balance).toEqual(50);
   });
